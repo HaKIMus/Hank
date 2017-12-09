ef759b4d238be46553b95c34d07a0f3febbe41c3
HaKIMus <webdevhakim@gmail.com>
2017-12-09 15:16:54 +0100

    Init

--- src/UI/Symfony/Controller/SignIn.php
+++ src/UI/Symfony/Controller/SignIn.php
@@ -6,10 +6,10 @@ namespace App\UI\Symfony\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class BankHomePage extends Controller
+class SignIn extends Controller
 {
     public function index(): Response
     {
-        return $this->render('main/homepage.twig');
+        return $this->render('authorization/sign-in.twig');
     }
 }
