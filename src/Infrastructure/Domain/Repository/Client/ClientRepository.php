5a437a3cb6ac981cbf521b9070cbac68d56a39e4
HaKIMus <webdevhakim@gmail.com>
2017-12-11 08:10:52 +0100

    Init

---
+++ src/Infrastructure/Domain/Repository/Client/ClientRepository.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hakim
+ * Date: 09.12.17
+ * Time: 14:27
+ */
+
+namespace App\Infrastructure\Domain\Repository\Client;
+
+use App\Domain\Client\Client;
+use App\Domain\Client\ClientRepositoryInterface;
+use App\Domain\Client\Exception\ClientNotFoundException;
+use App\Infrastructure\DoctrineRepositoryAbstract;
+use Ramsey\Uuid\Uuid;
+
+class ClientRepository extends DoctrineRepositoryAbstract implements ClientRepositoryInterface
+{
+    public function add(Client $client): void
+    {
+        $this->entityManager->persist($client);
+        $this->entityManager->flush();
+    }
+
+    public function getById(Uuid $id): Client
+    {
+        $client = $this->entityManager->getRepository(Client::class)
+        ->findOneBy([
+            'id' => $id
+        ]);
+
+        if ($client === null) {
+            throw new ClientNotFoundException();
+        }
+
+        return $client;
+    }
+
+    public function remove(Client $client): void
+    {
+        $this->entityManager->remove($client);
+    }
+
+    public function contains(Client $client): bool
+    {
+        return $this->entityManager->contains($client);
+    }
+}
