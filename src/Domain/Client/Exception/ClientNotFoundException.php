aa3a5188190fdb5a43a5012be66e1aa589c54db2
HaKIMus <webdevhakim@gmail.com>
2017-12-11 08:08:47 +0100

    Init

---
+++ src/Domain/Client/Exception/ClientNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hakim
+ * Date: 10.12.17
+ * Time: 16:34
+ */
+
+namespace App\Domain\Client\Exception;
+
+class ClientNotFoundException extends \Exception {}
