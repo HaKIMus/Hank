2fa26c685bed082d00cf0163b3543be63b8dfffe
HaKIMus <webdevhakim@gmail.com>
2017-12-16 11:04:42 +0100

    Init

---
+++ src/Application/Authorization/Exception/ClientNotSignedIn.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hakim
+ * Date: 14.12.17
+ * Time: 22:01
+ */
+
+namespace App\Application\Authorization\Exception;
+
+class ClientNotSignedIn extends \Exception {}
