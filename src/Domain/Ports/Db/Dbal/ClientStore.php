756e2e75c062510a02020f8c5126ea6f1e341011
HaKIMus <webdevhakim@gmail.com>
2018-01-03 07:01:26 +0100

    Moved to App\Domain\Ports\Db\Dbal namespace

---
+++ src/Domain/Ports/Db/Dbal/ClientStore.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hakim
+ * Date: 03.01.18
+ * Time: 06:23
+ */
+
+namespace App\Domain\Ports\Db\Dbal;
+
+use App\Application\Query\View\Client\ClientView;
+use Ramsey\Uuid\UuidInterface;
+
+interface ClientStore
+{
+    public function getById(UuidInterface $id): ClientView;
+    public function getAll(): array;
+    public function getByClientName(string $name): ClientView;
+}
