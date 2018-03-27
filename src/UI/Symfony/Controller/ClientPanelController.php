095cf7a70667e94d8237e974bdb9102dfe15b9bb
HaKIMus <webdevhakim@gmail.com>
2018-03-27 11:58:46 +0200

    Changing panel background feature

--- src/UI/Symfony/Controller/ClientPanelController.php
+++ src/UI/Symfony/Controller/ClientPanelController.php
@@ -2,15 +2,30 @@
 
 namespace Hank\UI\Symfony\Controller;
 
-use Hank\Application\Authorization\Exception\ClientNotSignedIn;
-use Hank\Infrastructure\Service\ClientService;
+use Hank\Infrastructure\Service\ChangeBackgroundService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ClientPanelController extends Controller
 {
     public function index(): Response
     {
-        return $this->render('panel/client-panel.twig');
+        return $this->render('panel/client-panel.twig', [
+            'changeBackgroundError' => $this->get('session')->getFlashBag()->get('changeBackgroundError')
+        ]);
+    }
+
+    public function changeClintBackground(Request $request, ChangeBackgroundService $changeBackgroundService): Response
+    {
+        try {
+            $changeBackgroundService->change($request->get('url'), $this->getUser()->getId());
+        } catch (\Exception $exception) {
+            $this->get('session')->getFlashBag()->add('changeBackgroundError', 'The URL is not an image!');
+
+            return $this->redirectToRoute('app_bank_client_panel');
+        }
+
+        return $this->redirectToRoute('app_bank_client_panel');
     }
 }
