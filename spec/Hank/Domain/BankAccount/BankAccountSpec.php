cbd1561d2d78fd81b82754888ad79e7d22717048
HaKIMus <webedevhakim@gmail.com>
2018-04-17 19:40:29 +0200

    Tests updated

--- spec/Hank/Domain/BankAccount/BankAccountSpec.php
+++ spec/Hank/Domain/BankAccount/BankAccountSpec.php
@@ -7,8 +7,11 @@ use Hank\Domain\BankAccount\BankAccount;
 use Hank\Domain\BankAccount\Exception\NegativeAmountOfMoneyException;
 use Hank\Domain\BankAccount\Exception\NoAmountOfMoneyException;
 use Hank\Domain\BankAccount\Exception\TooLargeAmountOfMoneyException;
+use Hank\Domain\BankAccount\Exception\TooSmallAmountOfMoneyException;
 use Hank\Domain\Client\Email;
 use Hank\Domain\Ports;
+use Hank\Domain\Ports\PayIn;
+use Hank\Domain\Ports\PayOut;
 use Hank\Infrastructure\Domain\Repository\LogRepository;
 use Money\Currency;
 use PhpSpec\ObjectBehavior;
@@ -29,18 +32,73 @@ class BankAccountSpec extends ObjectBehavior
 
     function it_allows_us_to_pay_in_money($payInPort, $logSystem): void
     {
-        $payInPort->implement(Ports\PayIn::class);
+        $payInPort->implement(PayIn::class);
         $logSystem->beADoubleOf(LogRepository::class);
 
-        $this->payIn(20.00, Uuid::uuid4(), $payInPort, $logSystem);
+        $this->payIn(20, Uuid::uuid4(), $payInPort, $logSystem);
+    }
+
+    function it_throws_exception_when_amount_is_null($payInPort, $logSystem): void
+    {
+        $payInPort->implement(PayIn::class);
+        $logSystem->beADoubleOf(LogRepository::class);
+
+        $this->shouldThrow(NoAmountOfMoneyException::class)
+            ->during("payIn", [0, Uuid::uuid4(), $payInPort, $logSystem]);
+    }
+
+
+    function it_throws_exception_when_amount_is_negative($payInPort, $logSystem): void
+    {
+        $payInPort->implement(PayIn::class);
+        $logSystem->beADoubleOf(LogRepository::class);
+
+        $this->shouldThrow(NegativeAmountOfMoneyException::class)
+            ->during("payIn", [-20, Uuid::uuid4(), $payInPort, $logSystem]);
+    }
+
+    function it_throws_exception_when_amount_is_smaller_than_5($payInPort, $logSystem): void
+    {
+        $payInPort->implement(PayIn::class);
+        $logSystem->beADoubleOf(LogRepository::class);
+
+        $this->shouldThrow(TooSmallAmountOfMoneyException::class)
+            ->during("payIn", [4, Uuid::uuid4(), $payInPort, $logSystem]);
+    }
+
+    function it_throws_exception_when_amount_is_greater_than_10000($payInPort, $logSystem): void
+    {
+        $payInPort->implement(PayIn::class);
+        $logSystem->beADoubleOf(LogRepository::class);
+
+        $this->shouldThrow(TooLargeAmountOfMoneyException::class)
+            ->during("payIn", [10001, Uuid::uuid4(), $payInPort, $logSystem]);
     }
 
     function it_allows_us_to_pay_out_money($payOutPort, $logSystem): void
     {
-        $payOutPort->implement(Ports\PayOut::class);
+        $payOutPort->implement(PayOut::class);
         $logSystem->beADoubleOf(LogRepository::class);
 
-        $this->payOut(20.00, Uuid::uuid4(), $payOutPort, $logSystem);
+        $this->payOut(20, Uuid::uuid4(), $payOutPort, $logSystem);
+    }
+
+    function it_throws_exception_when_amount_is_null_pay_out_money($payOutPort, $logSystem): void
+    {
+        $payOutPort->implement(PayOut::class);
+        $logSystem->beADoubleOf(LogRepository::class);
+
+        $this->shouldThrow(NoAmountOfMoneyException::class)
+            ->during('payOut', [0, Uuid::uuid4(), $payOutPort, $logSystem]);
+    }
+
+    function it_throws_exception_when_balance_after_transcation_will_be_less_than_minus_100($payOutPort, $logSystem): void
+    {
+        $payOutPort->implement(PayOut::class);
+        $logSystem->beADoubleOf(LogRepository::class);
+
+        $this->shouldThrow(TooLargeAmountOfMoneyException::class)
+            ->during('payOut', [200, Uuid::uuid4(), $payOutPort, $logSystem]);
     }
 
     function it_allows_us_to_send_money_to_friend($sendingMoneyPort, $logSystem): void
